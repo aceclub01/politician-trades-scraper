a3906778a434413b38a7f9731c3d607a6daafeb5
aceclub01 <acelee@dbs.com>
2025-03-04 21:37:18 +0800

    Update server.js

--- server.js
+++ server.js
@@ -52,6 +52,19 @@ app.get('/fxdata', async (req, res) => {
     }
 });
 
+app.get('/fetchfinancialgrowth', async (req, res) => {
+    const { symbol } = req.query;
+    const url = `https://financialmodelingprep.com/api/v3/financial-growth/${symbol}?apikey=${FMP_API_KEY}`;
+
+    try {
+        const response = await axios.get(url);
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error fetching key statistics:', error.message);
+        res.status(500).json({ error: 'Failed to fetch key financial growth data' });
+    }
+});
+
 app.get('/fetchKeyStatistics', async (req, res) => {
     const { symbol } = req.query;
     const url = `https://financialmodelingprep.com/api/v3/key-metrics/${symbol}?apikey=${FMP_API_KEY}`;
